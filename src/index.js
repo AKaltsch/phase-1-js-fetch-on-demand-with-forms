a6898d8a8fb5057b0e537aeb15182803b15ce464
alec <kaltschmidalec@gmail.com>
2021-07-26 20:37:55 -0700

    commit tree 0f97a988c57a3155492958f567017975c4b7085c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
+  const inputform = document.querySelector('form')
+  inputform.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // both of these ways can be used to select the input value
+
+    const input = document.querySelector('input#searchByID')
+    //console.log(event.target.children[1].value)
+
+    //console.log(input.value)
+
+
+    //grab and create a movie array from the movies database
+    //
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(movie => {
+        const title = document.querySelector('#movieDetails h4')
+        const summary = document.querySelector('#movieDetails p')
+        
+        title.innerText = movie.title
+        summary.textContent = movie.summary
+    });
+
+ });
 }
 
 document.addEventListener('DOMContentLoaded', init);
